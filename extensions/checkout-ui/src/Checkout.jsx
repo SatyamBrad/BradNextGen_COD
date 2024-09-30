cce6109ec93779b11dc9a3c398d681de063a0605
stymjhrbd <satyamjhrbd@gmail.com>
2024-09-30 12:02:28 +0530

    first commit

---
+++ extensions/checkout-ui/src/Checkout.jsx
@@ -0,0 +1,49 @@
+import {
+  reactExtension,
+  useApi,
+  useApplyAttributeChange,
+  BlockStack,
+  Button,
+  Heading,
+  Text,
+} from "@shopify/ui-extensions-react/checkout";
+import { useState } from "react";
+
+export default reactExtension("purchase.checkout.payment-method-list.render-before", () => (
+  <Extension />
+));
+
+function Extension() {
+  const { extension } = useApi();
+  const applyAttributeChange = useApplyAttributeChange();
+  const [selectedPayment, setSelectedPayment] = useState("");
+
+
+
+  const handlePaymentSelection = async (method) => {
+    setSelectedPayment(method);
+
+    try {
+      const result = await applyAttributeChange({
+        key: "payment_method",
+        type: "updateAttribute",
+        value: method,
+      });
+      if (result.success) {
+        console.log(`Payment method '${method}' selected, checkout updated.`);
+      } else {
+        throw new Error(result.message || "Failed to update payment method.");
+      }
+    } catch (error) {
+      console.error("Error updating checkout with COD:", error);
+      alert("Sorry, we couldn't update the payment method. Please try again.");
+    }
+  };
+
+  return (
+    <BlockStack spacing="loose">
+
+
+    </BlockStack>
+  );
+}
